bffaf9b3ce58b069532fb09a3fb5e14479713b6a
OtterBlue <96080748+OtterBlue@users.noreply.github.com>
2023-05-26 17:05:52 +0200

    obtener IDs primera película ciclo (para imagen miniatura)

--- app/src/app/public/pages/inicio/inicio.component.ts
+++ app/src/app/public/pages/inicio/inicio.component.ts
@@ -12,6 +12,8 @@ export class InicioComponent implements OnInit {
   ciclos: Ciclo[] = [];
   ciclosLength: number = 0;
   ciclosRandom: number[] = [];
+  idPeli: string = "";
+  idExternos: string[] = [];
 
   constructor (
     private ciclosService: CiclosService,
@@ -23,6 +25,11 @@ export class InicioComponent implements OnInit {
         this.ciclosLength = data.length; // Asignar la longitud de los ciclos
 
         this.ciclosRandom = this.randomCiclos(); // Llamar a randomCiclos() después de recibir los ciclos
+        this.idExternos = this.getFirstMovie(); // Obtener los idExterno de las primeras películas
+
+        // Llamar a la función para obtener información adicional de las películas utilizando los idExternos
+        this.getMovieInformation(this.idExternos);
+        console.log('getMovieInformation() llamado con idExternos:', this.idExternos);
       }
     );
   }
@@ -40,20 +47,40 @@ export class InicioComponent implements OnInit {
       if (!ciclosRandom.includes(randomIndex)) {
         ciclosRandom.push(randomIndex);
       }
+    }
+
+    console.log("ciclos random: " + ciclosRandom.join(" "));
+
+    return ciclosRandom;
   }
 
-  console.log("ciclos random: " + ciclosRandom.join(" "));
+  getFirstMovie() {
+    const idExternos: string[] = [];
 
-  return ciclosRandom;
+    for (let i = 0; i < 3; i++) {
+      const ciclo = this.ciclos[this.ciclosRandom[i]];
+      if (ciclo && ciclo.peliculas && ciclo.peliculas.length > 0) {
+        const primeraPelicula = ciclo.peliculas[0];
+        const idExterno = primeraPelicula.idExterno;
+        if (idExterno) {
+          idExternos.push(idExterno);
+        }
+      }
+    }
+
+    return idExternos;
   }
 
+  getMovieInformation(idExternos: string[]) {
+    // Aquí puedes llamar a la API para obtener información adicional de las películas utilizando los idExternos
+    // Puedes implementar la lógica para realizar las solicitudes a la API y mostrar la información en tu componente
+  }
 
   /*
   1.Coger id del ciclo,
   2. Hacer una petición a la API para obtener el ciclo con ese id
   3. Mostrar datos películas
-  4. La primera de cada ciclo obtener de API movies la info en concreto la imagen
-  5. Mostrar imagen
-  */
+  4. La primera de cada ciclo obtener de
+*/
 
 }
