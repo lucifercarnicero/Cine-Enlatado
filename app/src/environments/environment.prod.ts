f1c340c5b6da8a3a149c34fac5636a9bcb46eb1a
OtterBlue <96080748+OtterBlue@users.noreply.github.com>
2023-06-11 13:07:28 +0200

    rebuild

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://44.203.59.138:3000/api'
+  baseUrl: 'http://44.203.59.138:3000/api',
+  imdbUrl: 'https://imdb-api.com/en/API',
 };
