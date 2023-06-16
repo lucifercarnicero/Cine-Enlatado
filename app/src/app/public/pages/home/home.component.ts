47940c475fdc91c374dfe211143fe23597cba4de
Luci Fer <114153648+lucifercarnicero@users.noreply.github.com>
2023-06-16 13:21:03 +0200

    Update home.component.ts

--- app/src/app/public/pages/home/home.component.ts
+++ app/src/app/public/pages/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  konamiCode: string[] = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'Enter'];
+  currentPosition = 0;
 
+  constructor(private router: Router) { }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyDown(event: KeyboardEvent) {
+    if (event.key === this.konamiCode[this.currentPosition]) {
+      this.currentPosition++;
+
+      if (this.currentPosition === this.konamiCode.length) {
+        this.currentPosition = 0;
+        this.router.navigate(['/home/list']); // Reemplaza '/otra-pagina' por la ruta deseada
+
+      }
+    } else {
+      this.currentPosition = 0;
+    }
+  }
 }
+
+
